8b321020f660f8a9055f12aeb6f0e132dadaa88a
shotaro <hiranoshotaro@gmail.com>
2016-09-12 01:03:22 +0900

    update

--- js/myjs.js
+++ js/myjs.js
@@ -22,6 +22,7 @@ jQuery(function($){
 		$('#more').hide();
 	});
 });
+// twitter widgets
 !function(d,s,id){
 	var js,fjs=d
 	.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
@@ -31,3 +32,10 @@ jQuery(function($){
 		fjs.parentNode.insertBefore(js,fjs);
 	}
 }(document, 'script', 'twitter-wjs');
+// google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-83978436-1', 'auto');
+ga('send', 'pageview');
